5575c46432d278202229904df274c98988c0f494
BountifulGames <hunter.wright98@yahoo.com>
2024-04-23 18:26:25 -0400

    Line of site fixed

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,10 +8,11 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private float chaseSpeed = 5.0f;
     [SerializeField] private float patrolSpeed = 2.0f;
     [SerializeField] private float sightDistance = 10.0f;
-    [SerializeField] private float fieldOfViewAngle = 110f;
+    [SerializeField] private float fieldOfViewAngle = 90f;
+    [SerializeField] private Transform playerPos;
+
     private Vector3 lastKnownLocation;
     private EnemyController enemyController;
-    private Transform playerPos;
     private int currentWaypoint = 0;
     private bool isInvestigating = false;
     
@@ -26,6 +27,8 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         StateChange();
+        LookForPlayer();
+
     }
 
     private void StateChange()
@@ -46,15 +49,19 @@ public class EnemyAI : MonoBehaviour
 
     private void Patrol()
     {
+        Debug.Log("Patroling");
+        enemyController.MoveTo(waypoints[currentWaypoint].position, patrolSpeed);
         if (Vector3.Distance(gameObject.transform.position, waypoints[currentWaypoint].position) < 1.0f)
         {
             currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
         }
-        enemyController.MoveTo(waypoints[currentWaypoint].position, patrolSpeed);
+
     }
 
     private void Chase()
     {
+        StopCoroutine(PerformInvestigation());
+        Debug.Log("Chasing");
         enemyController.MoveTo(playerPos.position, chaseSpeed);
         if (Vector3.Distance(gameObject.transform.position, playerPos.position) > sightDistance || !CanSeePlayer())
         {
@@ -65,9 +72,12 @@ public class EnemyAI : MonoBehaviour
 
     private void Investigate()
     {
+        //Debug.Log("Investigating");
+        Debug.Log("Last known location " + lastKnownLocation.ToString());
         enemyController.MoveTo(lastKnownLocation, patrolSpeed);
-        if (Vector3.Distance(gameObject.transform.position, lastKnownLocation) < 1f && !isInvestigating)
+        if (Vector3.Distance(gameObject.transform.position, lastKnownLocation) <= 1.5f && !isInvestigating)
         {
+            Debug.Log("Beginning investigation");
             StartCoroutine(PerformInvestigation());
         }
     }
@@ -81,10 +91,22 @@ public class EnemyAI : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(transform.position + transform.up, playerDirection.normalized, out hit, sightDistance))
             {
+                Debug.DrawRay(transform.position, playerDirection.normalized, Color.red);
+
+                Debug.Log("Raycast Hit " + hit.collider.name);
                 return hit.transform == playerPos;
             }
         }
         return false;
+    }
+
+    private void LookForPlayer()
+    {
+        if (CanSeePlayer() && Vector3.Distance(playerPos.position, gameObject.transform.position) < sightDistance)
+        {
+            lastKnownLocation = playerPos.position;
+            enemyState = EnemyState.Chasing;
+        }
     }
 
     private IEnumerator PerformInvestigation()
@@ -95,5 +117,23 @@ public class EnemyAI : MonoBehaviour
 
         isInvestigating = false;
         enemyState = EnemyState.Patrolling;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(gameObject.transform.position, sightDistance);
+        Vector3 fovLine1 = Quaternion.AngleAxis(fieldOfViewAngle / 2, transform.up) * transform.forward * sightDistance;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-fieldOfViewAngle / 2, transform.up) * transform.forward * sightDistance;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
+
+        if (CanSeePlayer())
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, playerPos.position);
+        }
     }
 }
